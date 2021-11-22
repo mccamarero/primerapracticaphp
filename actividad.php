4da4a1b37874f525472e26113b23c1f5b4c2512c
mccamarero <56662779+mccamarero@users.noreply.github.com>
2021-11-22 12:53:47 -0300

    Add files via upload

---
+++ actividad.php
@@ -0,0 +1,62 @@
+
+
+
+<?php
+$nombre = "Hola, mi nombre es MarIa Cecilia";
+echo $nombre;
+$apellido = "y mi apellido es Camarero";
+$edad = "En el fondo de mi corazOn sigo teniendo 20 años, ¡jaja!";
+$hobbies = "Les cuento que uno de mis hobbies es hacer bijouterie";
+$editor_preferido = "Por supuesto que mi editor de código preferido es Visual";
+$sistema_operativo = "El sistema operativo que utilizo es Windows";
+$practica = "Estuve practicando los arrays asociativos, asI que aquI tiro algo ... a ver si sale:";
+$megusta = " Les cuento en arrays cosas que me gustan y cosas que no:";
+
+$pintor = [
+    "id_pintor" => "Leonardo Da Vinci",
+    "obra1" => "La Mona Lisa",
+    "obra2" => "La dama con armiño",
+    "informacion" =>[
+        "tipo" => "son solo algunos de los retratos que me gustan de Leonardo"
+    ]
+
+];
+
+$bandas = [
+    "id_bandas" => "Queen",
+    "cancion1" => "Don´t stop me now",
+    "cancion2" => "Under pressure",
+    "cancion3" => "Save me",
+        "informacion" =>[
+            "tipo" => "son solo algunas de mis canciones favoritas de Queen"
+        ]
+];
+
+
+
+
+echo $apellido;
+echo "<br>";
+echo $edad;
+echo "<br>";
+echo $hobbies;
+echo "<br>";
+echo $editor_preferido;
+echo "<br>";
+echo $sistema_operativo;
+echo "<br>";
+echo "<br>";
+echo $practica;
+
+echo "<br>";
+
+var_dump($pintor["id-pintor
+
+"]);
+
+
+echo $arte;
+echo $musica;
+
+
+?>
---
+++ actividadPHP1.php
@@ -0,0 +1,47 @@
+
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PruebaTickets</title>
+ 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    
+
+<title>TPintegradorFinal</title>   
+
+
+</head>
+<body>
+<h1>Esta fue mi primera prueba de PHP con HTML</h1>
+</body>
+</html>
+
+
+ 
+<div class="titulos">
+     
+      <h2 style="text-align: center;">REGISTRO</h2>
+</div>
+
+
+$nombre = "Hola, mi nombre es MarIa Cecilia";
+echo $nombre;
+$apellido = "y mi apellido es Camarero";
+$edad = "En el fondo de mi corazOn sigo teniendo 20 años, ¡jaja!";
+$hobbies = "Les cuento que uno de mis hobbies es hacer bijouterie";
+$editor_preferido = "Por supuesto que mi editor de código preferido es Visual";
+$sistema_operativo = "El sistema operativo que utilizo es Windows";
+$practica = "Estuve practicando los arrays asociativos, asI que aquI tiro algo ... a ver si sale:";
+$megusta = " Les cuento en arrays cosas que me gustan y cosas que no:";
+
+
+
+
+
