bc57e8bbf27706dcaba688855da2072cc0218816
mccamarero <56662779+mccamarero@users.noreply.github.com>
2021-11-23 09:15:06 -0300

    Update actividad.php

--- actividad.php
+++ actividad.php
@@ -10,53 +10,6 @@ $hobbies = "Les cuento que uno de mis hobbies es hacer bijouterie";
 $editor_preferido = "Por supuesto que mi editor de código preferido es Visual";
 $sistema_operativo = "El sistema operativo que utilizo es Windows";
 $practica = "Estuve practicando los arrays asociativos, asI que aquI tiro algo ... a ver si sale:";
-$megusta = " Les cuento en arrays cosas que me gustan y cosas que no:";
-
-$pintor = [
-    "id_pintor" => "Leonardo Da Vinci",
-    "obra1" => "La Mona Lisa",
-    "obra2" => "La dama con armiño",
-    "informacion" =>[
-        "tipo" => "son solo algunos de los retratos que me gustan de Leonardo"
-    ]
-
-];
-
-$bandas = [
-    "id_bandas" => "Queen",
-    "cancion1" => "Don´t stop me now",
-    "cancion2" => "Under pressure",
-    "cancion3" => "Save me",
-        "informacion" =>[
-            "tipo" => "son solo algunas de mis canciones favoritas de Queen"
-        ]
-];
-
-
-
-
-echo $apellido;
-echo "<br>";
-echo $edad;
-echo "<br>";
-echo $hobbies;
-echo "<br>";
-echo $editor_preferido;
-echo "<br>";
-echo $sistema_operativo;
-echo "<br>";
-echo "<br>";
-echo $practica;
-
-echo "<br>";
-
-var_dump($pintor["id-pintor
-
-"]);
-
-
-echo $arte;
-echo $musica;
-
+$megusta = " Despues les cuento en arrays cosas que me gustan y cosas que no:";
 
 ?>
